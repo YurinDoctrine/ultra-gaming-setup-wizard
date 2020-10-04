aa7e6930655b3b23f3506d9b977dfdc140423381
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2020-10-04 14:54:24 +0300

    Add files via upload

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -110,7 +110,7 @@ prompt_2() {
 	sudo apt update
 	sudo apt-get install --install-recommends winehq-staging -y
 	sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
-	sudo apt-get install --install-recommends dxvk lutris -y
+	sudo apt-get install --install-recommends dxvk lutris ubuntu-restricted-extras -y
 	clear
 
 }
@@ -137,6 +137,7 @@ utulities() {
 	sudo add-apt-repository ppa:linrunner/tlp
 	sudo apt update
 	sudo apt install gamemode earlyoom tlp tlp-rdw zram-tools amd64-microcode iucode-tool intel-microcode -y
+	sudo apt --purge remove gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-preferences -y
 	sudo tlp start
 	sudo sysctl -w vm.swappiness=1
 	echo 'vm.swappiness=1'| sudo tee /etc/sysctl.d/local.conf
