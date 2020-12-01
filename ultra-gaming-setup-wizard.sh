29a376e30e78863e63b3ef0dd96134a5b5ca91f8
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2020-12-01 15:57:09 +0300

    Update ultra-gaming-setup-wizard.sh

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -209,7 +209,7 @@ prompt_2() {
 	which pacman >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lutris -y
-		yay -S --needed --noconfirm ninja meson glslang systemd git dbus base-devel dxvk-bin preload vkbasalt
+		yay -S --needed --noconfirm ninja meson glslang systemd git dbus base-devel dxvk-bin vkbasalt
 		cd
 		git clone https://github.com/Frogging-Family/wine-tkg-git.git
 		cd wine-tkg-git/
