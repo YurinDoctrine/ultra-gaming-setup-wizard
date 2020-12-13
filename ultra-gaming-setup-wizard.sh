1e0ef07e9182d85df7ff750895d84a7f63a89191
YurinDoctrine <bayramyurgidenn@gmail.com>
2020-12-13 13:58:06 +0300

    Add files via upload

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -9,13 +9,13 @@ if [ $? -eq 0 ]; then
 	echo -e "╚═══════════════════════════════════════════════════╝"
 
 else
-        clear
-        echo -e "╔═══════════════════════════════════════════════════╗"
-        echo -e "║THIS SCRIPT ONLY WORKS ON ARCH&UBUNTU BASED DISTROS║"
-        echo -e "║                                                   ║"
-        echo -e "╚═══════════════════════════════════════════════════╝"
-        echo -e ""
-        exit 1
+	clear
+	echo -e "╔═══════════════════════════════════════════════════╗"
+	echo -e "║THIS SCRIPT ONLY WORKS ON ARCH&UBUNTU BASED DISTROS║"
+	echo -e "║                                                   ║"
+	echo -e "╚═══════════════════════════════════════════════════╝"
+	echo -e ""
+	exit 1
 fi
 which pacman >/dev/null 2>&1
 if [ $? -eq 0 ]; then
