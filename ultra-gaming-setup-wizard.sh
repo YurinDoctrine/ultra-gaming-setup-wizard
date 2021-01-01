43072375f630d63ec36966b99a096f75d1b8f5a2
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-01-01 18:51:24 +0300

    Add files via upload

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -242,7 +242,8 @@ prompt_2() {
 		sudo apt update
 		sudo apt install --install-recommends winehq-staging -y
 		sudo apt install --install-recommends libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
-		sudo apt install build-essential manpages-dev libx11-dev ninja xorg-dev meson glslang dbus dxvk steam lutris -y
+		sudo apt install --install-recommends build-essential manpages-dev libx11-dev ninja-build xorg-dev meson dbus dxvk steam lutris -y
+		sudo apt install glslang -y
 		cd
 		git clone https://github.com/DadSchoorse/vkBasalt.git
 		cd vkBasalt/
