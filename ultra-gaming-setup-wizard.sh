845d9e59d97d0a3f0026058acee11569b0aa4120
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-01-22 11:55:00 +0300

    Add files via upload

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -128,7 +128,7 @@ xanmod() {
 liquarix() {
     which apt >/dev/null 2>&1
     if [ $? -eq 0 ]; then
-        sudo add-apt-repository ppa:damentz/liquorix &&
+        sudo add-apt-repository ppa:damentz/liquorix -y &&
             sudo apt update
         sudo apt install linux-image-liquorix-amd64 linux-headers-liquorix-amd64 -y
     fi
@@ -203,8 +203,8 @@ prompt_2() {
         sudo add-apt-repository ppa:lutris-team/lutris -y
         sudo apt update
         sudo apt install winehq-staging -y
-        sudo apt install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
-        sudo apt install build-essential manpages-dev libx11-dev xorg-dev dbus dxvk steam lutris -y
+        sudo apt install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 steam -y
+        sudo apt install build-essential manpages-dev libx11-dev xorg-dev dbus dxvk lutris -y
         cd &&
             git clone https://github.com/DadSchoorse/vkBasalt.git &&
             cd vkBasalt/ &&
