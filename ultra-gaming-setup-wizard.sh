47d0d2b737a272251ee9a89e056ea325425f2798
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-02-24 14:03:32 +0300

    Add files via upload

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -202,7 +202,7 @@ prompt_2() {
     which apt >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         cd /tmp &&
-            wget -nc https://dl.winehq.org/wine-builds/winehq.key &&
+            wget -qnc https://dl.winehq.org/wine-builds/winehq.key &&
             sudo apt-key add winehq.key &&
             sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y &&
             cd
@@ -243,51 +243,16 @@ prompt_4() {
     which apt >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         sudo apt install --install-recommends gamemode -y &&
-            curl https://raw.githubusercontent.com/FeralInteractive/gamemode/master/example/gamemode.ini >~/gamemode.ini && sudo mv ~/gamemode.ini /etc/gamemode.ini
-        sudo apt install --install-recommends thermald powertop irqbalance haveged profile-sync-daemon schedtool -y
-        sudo powertop --auto-tune
-        sudo add-apt-repository ppa:oibaf/test -y &&
-            sudo apt update &&
-            sudo apt install --install-recommends -y nohang &&
-            sudo systemctl enable --now nohang-desktop.service
-        cd /tmp &&
-            git clone https://github.com/AdnanHodzic/auto-cpufreq.git &&
-            cd auto-cpufreq/ &&
-            sudo ./auto-cpufreq-installer &&
-            sudo auto-cpufreq --install &&
-            cd /tmp
-        git clone https://github.com/hakavlad/memavaild.git &&
-            cd memavaild/ &&
-            deb/build.sh &&
-            sudo apt install --install-recommends -y --reinstall ./deb/package.deb &&
-            sudo systemctl enable --now memavaild.service &&
-            cd /tmp
-        git clone https://github.com/Nefelim4ag/Ananicy.git &&
-            ./Ananicy/package.sh debian &&
-            sudo dpkg -i ./Ananicy/ananicy-*.deb &&
-            sudo systemctl enable --now ananicy
-        git clone https://github.com/hakavlad/prelockd.git &&
-            cd prelockd/ &&
-            deb/build.sh &&
-            sudo apt install --install-recommends -y --reinstall ./deb/package.deb &&
-            sudo systemctl enable --now prelockd.service &&
-            cd /tmp
-        git clone https://github.com/Nefelim4ag/systemd-swap.git &&
-            cd systemd-swap &&
-            make deb &&
-            sudo apt install --install-recommends -y ./systemd-swap_*_all.deb &&
-            cd
-        sudo add-apt-repository ppa:x3n0m0rph59/precached -y &&
-            sudo apt update &&
-            sudo apt install --install-recommends -y precached
+            wget -qnc https://raw.githubusercontent.com/YurinDoctrine/ultra-gaming-setup-wizard/main/gamemode.ini &&
+            sudo mv ~/gamemode.ini /etc/gamemode.ini
+        sudo apt install --install-recommends thermald powertop -y
     fi
     which pacman >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         yay -S --needed --noconfirm gamemode lib32-gamemode &&
-            curl https://raw.githubusercontent.com/FeralInteractive/gamemode/master/example/gamemode.ini >~/gamemode.ini && sudo mv ~/gamemode.ini /etc/gamemode.ini
-        yay -S --needed --noconfirm powertop
-        sudo powertop --auto-tune
-        yay -S --needed performance-tweaks
+            wget -qnc https://raw.githubusercontent.com/YurinDoctrine/ultra-gaming-setup-wizard/main/gamemode.ini &&
+            sudo mv ~/gamemode.ini /etc/gamemode.ini
+        yay -S --needed --noconfirm powertop thermald
     fi
 }
 prompt_4
