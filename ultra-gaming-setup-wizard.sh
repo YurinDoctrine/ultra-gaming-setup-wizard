933aaf9079fb7e35a3fa649b0b166581fc996e7c
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-05-06 22:37:10 +0300

    Add files via upload

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -206,8 +206,7 @@ prompt_2() {
             wget -qnc https://dl.winehq.org/wine-builds/winehq.key &&
             sudo apt-key add winehq.key &&
             sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y &&
-            cd
-        sudo add-apt-repository ppa:lutris-team/lutris -y
+            sudo add-apt-repository ppa:lutris-team/lutris -y
         sudo apt-get update
         sudo apt install --install-recommends winehq-staging -y
         sudo apt install --install-recommends libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
