8b2b28f8feca2867619021898accc850f54d4528
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-05-11 12:58:41 +0300

    Add files via upload

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -255,7 +255,7 @@ prompt_4
 
 extra() {
     curl -fsSL https://raw.githubusercontent.com/YurinDoctrine/secure-linux/master/secure.sh >secure.sh &&
-        chmod 755 secure.sh &&
+        chmod 0755 secure.sh &&
         ./secure.sh
 }
 
