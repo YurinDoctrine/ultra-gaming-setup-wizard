a38f900949f0eb4b8aa8b4b89ca12fc66e2b51c2
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-09-14 21:35:59 +0300

    edit ultra-gaming-setup-wizard.sh

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -245,18 +245,16 @@ prompt_3() {
 prompt_3
 
 prompt_4() {
-    echo -e "NEXT YOU GOTTA INSTALL A COUPLE OF DAEMON EITHER (earlyoom, gamemode, thermald etc.) [HIT RETURN]"
+    echo -e "NEXT YOU GOTTA INSTALL A COUPLE OF DAEMON EITHER (gamemode, thermald etc.) [HIT RETURN]"
     read -p $'>_: '
     which apt >/dev/null 2>&1
     if [ $? -eq 0 ]; then
-        sudo apt install --assume-yes --install-recommends earlyoom -f
         sudo apt install --assume-yes --install-recommends gamemode -f
         sudo apt install --assume-yes --install-recommends thermald -f
         sudo apt install --assume-yes --install-recommends irqbalance -f
     fi
     which pacman >/dev/null 2>&1
     if [ $? -eq 0 ]; then
-        yay -S --needed --noconfirm earlyoom
         yay -S --needed --noconfirm gamemode lib32-gamemode
         yay -S --needed --noconfirm thermald
         yay -S --needed --noconfirm irqbalance
