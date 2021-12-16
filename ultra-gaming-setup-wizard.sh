65f3ccac0149222cbd1e5404bc5764a8209abcb5
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-12-16 14:50:07 +0300

    edit ultra-gaming-setup-wizard.sh

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -218,7 +218,8 @@ prompt_2() {
             sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y &&
             sudo add-apt-repository ppa:lutris-team/lutris -y
         sudo apt-get update
-        sudo apt install --assume-yes winehq-staging libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 kubuntu-restricted-extras -f
+        sudo apt install --assume-yes winehq-staging libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -f
+        sudo apt install -f --assume-yes kubuntu-restricted-extras
         sudo apt install -f --assume-yes --no-install-recommends dialog dosbox steam lutris q4wine winetricks
     fi
     which pacman >/dev/null 2>&1
