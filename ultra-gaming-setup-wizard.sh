ab40fe92d94edbdbad0636e578f6a4f6da504cf6
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-14 12:03:00 +0300

    edit ultra-gaming-setup-wizard.sh

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -111,9 +111,6 @@ nvidia() {
         sudo add-apt-repository ppa:graphics-drivers/ppa -y
         sudo apt-get update
         sudo apt install -f --assume-yes --no-install-recommends libgl1-mesa-dri libglx-mesa0 libglx-mesa0:i386 mesa-vulkan-drivers nvidia-driver-510 libnvidia-gl-510 libnvidia-gl-510:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 mesa-va-drivers mesa-va-drivers:i386 libvulkan-dev libvulkan-dev:i386 mesa-utils vulkan-tools mesa-common-dev &&
-            echo -e "WINE_FSR_OVERRIDE=1" | sudo tee -a /etc/environment &&
-            echo -e "WINE_FULLSCREEN_FSR=1" | sudo tee -a /etc/environment &&
-            echo -e "WINE_FULLSCREEN_FSR_STRENGTH=1" | sudo tee -a /etc/environment &&
             echo -e "DXVK_HUD=compile" | sudo tee -a /etc/environment &&
             echo -e "mesa_glthread=true" | sudo tee -a /etc/environment &&
             echo -e "__GL_THREADED_OPTIMIZATIONS=1" | sudo tee -a /etc/environment &&
@@ -123,9 +120,6 @@ nvidia() {
     which pacman >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         yay -S --needed --noconfirm mesa lib32-mesa nvidia nvidia-dkms opencl-nvidia lib32-opencl-nvidia nvidia-utils lib32-nvidia-utils xf86-video-nouveau libva-mesa-driver vulkan-tools &&
-            echo -e "WINE_FSR_OVERRIDE=1" | sudo tee -a /etc/environment &&
-            echo -e "WINE_FULLSCREEN_FSR=1" | sudo tee -a /etc/environment &&
-            echo -e "WINE_FULLSCREEN_FSR_STRENGTH=1" | sudo tee -a /etc/environment &&
             echo -e "DXVK_HUD=compile" | sudo tee -a /etc/environment &&
             echo -e "mesa_glthread=true" | sudo tee -a /etc/environment &&
             echo -e "__GL_THREADED_OPTIMIZATIONS=1" | sudo tee -a /etc/environment &&
