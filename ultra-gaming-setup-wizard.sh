cfaaf65c8057a9fa474ffcc0ff6713d3f52bf064
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-22 20:41:06 +0300

    edit ultra-gaming-setup-wizard.sh

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -86,7 +86,7 @@ multilib() {
     fi
     which dnf >/dev/null 2>&1
     if [ $? -eq 0 ]; then
-        sudo dnf search u
+        sudo dnf check-update
         sudo dnf install dnf-plugins-core -y
         sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     fi
