315f00d93e44cfbec0dc0ef98ac177e91a0ef9cd
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-05-04 19:43:26 +0300

    edit ultra-gaming-setup-wizard.sh

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -471,9 +471,10 @@ prompt_2() {
     which apt >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         cd /tmp &&
+
             wget -qnc https://dl.winehq.org/wine-builds/winehq.key &&
-            sudo apt-key add winehq.key &&
-            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y &&
+            sudo mv winehq.key /usr/share/keyrings/winehq-archive.key &&
+            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ jammy main' -y &&
             sudo add-apt-repository ppa:lutris-team/lutris -y
         sudo apt-get update
         sudo apt install --assume-yes winehq-staging libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -f
