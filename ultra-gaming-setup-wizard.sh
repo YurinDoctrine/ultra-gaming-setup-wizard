a972d8dcab2018a25e37ffac6e8431583335c7fd
Yurin Doctrine <doctriney@protonmail.com>
2022-09-19 10:52:29 +0300

    edit ultra-gaming-setup-wizard.sh

--- ultra-gaming-setup-wizard.sh
+++ ultra-gaming-setup-wizard.sh
@@ -532,7 +532,7 @@ prompt_2() {
             sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ jammy main' -y &&
             sudo add-apt-repository ppa:lutris-team/lutris -y
         sudo apt-get update
-        sudo apt install --assume-yes winehq-staging libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 lib fontconfig:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -f
+        sudo apt install --assume-yes winehq-staging libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 fontconfig:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -f
         sudo apt install -f --assume-yes kubuntu-restricted-extras
         sudo apt install -f --assume-yes --no-install-recommends dialog dosbox dxvk speedtest-cli steam hwloc lutris m4 usbutils make zenity winetricks
     fi
